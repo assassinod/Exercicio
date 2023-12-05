6ac83ce0b8de2c4b8a115db4cbb0627ad7a62cd7
Daniel oliveira <danield2game@gmail.com>
2023-12-05 20:06:13 -0300

    Pimeira alteração

---
+++ Primeiroprojeto/Primeiroprojeto/src/Primeiraclasse.java
@@ -0,0 +1,14 @@
+public class Primeiraclasse {
+
+    public static void main(String[] args) {
+            String ambiente = args[0];
+
+            if(ambiente.equals("Dev")) {
+                System.out.print("Dev");
+            }
+
+        }
+
+    }
+
+}
---
+++ Untitled-1.java
@@ -0,0 +1,14 @@
+
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args[0];
+																										
+		if(ambiente.equals("Dev")) {
+			System.out.print("Dev");
+		}
+
+	}
+
+}
